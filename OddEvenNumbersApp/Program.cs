2c3d0d7dbcf31bfec66c9ce026eb0217b5d60a45
mohamed <m.alkordy2003@gmail.com>
2024-03-27 14:20:18 +0200

    initial commit

---
+++ OddEvenNumbersApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace OddEvenNumbers
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Print("Numbers", numbers);
+            Print("Even Numbers", numbers.Where(x=>IsEven(x)));
+            Print("Odd Numbers", numbers.Where(x => IsOdd(x)));
+
+
+
+        }
+        static void Print(string title,IEnumerable<int>numbers)
+        {
+            Console.WriteLine();
+            Console.Write($"{title}: [ ");
+            foreach (var n in numbers)
+            {
+                Console.Write($"{n},");
+
+            }
+            Console.Write(" ]");
+            Console.WriteLine();
+        }
+        static bool IsOdd(int number)
+        {
+            if (number % 2 != 0)
+                return true;
+            else 
+                return false;
+        }
+        static bool IsEven(int number)
+        {
+            if (number % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
